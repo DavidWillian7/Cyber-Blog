800418a638f5739e4d5bb8a528c17a505b57828b
DavidWillian7 <85882885+DavidWillian7@users.noreply.github.com>
2023-04-23 14:44:47 -0300

    deixando os componentes dinamicos com dados que podem ser inputados na tag

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent {
+  @Input()
+  photoCard: string = "";
 
+  @Input()
+  cardTitle: string = "";
 }
